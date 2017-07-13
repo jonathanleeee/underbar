4fcf9f5fbe2bc1ddabe72af34774ac449d417917
Jonathan Lee <jonathanleeee@gmail.com>
2017-07-13 14:07:38 -0700

    finish filter

--- src/underbar.js
+++ src/underbar.js
@@ -84,6 +84,19 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(value) {
+      if(test(value)) {
+        result.push(value);
+      }
+    });
+    return result;
+    // for (var i = 0; i < collection.length; i++) {
+    //   if (test(collection[i])) {
+    //     result.push(collection[i]);
+    //   }
+    // }
+    //return result;    
   };
 
   // Return all elements of an array that don't pass a truth test.
