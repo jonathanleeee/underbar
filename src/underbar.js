45e7e00fcd5480bfd77834fdb36e3becef3eab20
Jonathan Lee <jonathanleeee@gmail.com>
2017-07-13 12:38:30 -0700

    finish each

--- src/underbar.js
+++ src/underbar.js
@@ -39,6 +39,13 @@
   // last element.
   _.last = function(array, n) {
     return n === undefined ? array[array.length - 1] : array.slice(Math.max(0, array.length-n));
+  //   if (n === undefined) {
+  //   return array[array.length -1];
+  // } else if (n === 0) {
+  //   return [];
+  // } else {
+  //   return array.slice(-n);
+  //   }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -47,6 +54,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if ( Array.isArray(collection)) {
+      for ( var i = 0; i < collection.length; i++ ) {
+        iterator(collection[i], i, collection)
+      }
+    } else {
+      for ( var key in collection) {
+        iterator(collection[key], key, collection)
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
